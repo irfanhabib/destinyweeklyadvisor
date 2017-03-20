3cc77d820dc893b2d9055e878233b1786e0f2d2a
Irfan Habib <irfan@irfanhabib.com>
2017-03-20 11:57:55 +0000

    Adding icons

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 /* eslint-disable  func-names */
 /* eslint quote-props: ["error", "consistent"]*/
-/**
- * This sample demonstrates a simple skill built with the Amazon Alexa Skills
- * nodejs skill development kit.
- * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
- * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
- * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
- **/
 
 'use strict';
 
@@ -17,12 +10,17 @@ var APP_ID = 'amzn1.ask.skill.b3c06f6f-0ed8-40de-bfcd-bec4fe3b26ce';
 
 var handlers = {
     'FetchWeeklyEvents': function () {
-        var speechOutput = AdvisorApi.fetchWeeklyData();
-        this.emit(':tell', speechOutput);
+      var that = this;
+      AdvisorApi.fetchWeeklyData().then(function(speechOutput){
+        that.emit(':tell', speechOutput);
+      })
     },
     'FetchDailyEvents': function () {
-        var speechOutput = AdvisorApi.fetchDailyData();
-        this.emit(':tell', speechOutput);    },
+      var that = this;
+      AdvisorApi.fetchDailyData().then(function(speechOutput){
+        that.emit(':tell', speechOutput);
+      })
+    },
     'AMAZON.StopIntent': function () {
         this.emit(':tell', this.t('STOP_MESSAGE'));
     },
